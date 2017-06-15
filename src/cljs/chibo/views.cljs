b249d0a11203fda51164b4eade3adeb0bb4e3ca8
feychou <federica.recanatini@gmail.com>
2017-06-15 20:22:19 +0200

    Refactor view

--- src/cljs/chibo/views.cljs
+++ src/cljs/chibo/views.cljs
@@ -6,39 +6,7 @@
     (fn []
       (case @panel
           "alphabet-choice" [alphabet-choice]
-          "quiz-options" [:div.container
-                      [:form
-                        [:input {:id "free-text-true"
-                                 :name "free-text"
-                                 :type "radio"
-                                 :value true
-                                 :on-click #(dispatch [:quiz-options-filtered {:free-text true}])}]
-                        [:label {:for "free-text-true"} "Free text"]
-                        [:input {:id "free-text-false"
-                                 :name "free-text"
-                                 :type "radio"
-                                 :value false
-                                 :on-click #(dispatch [:quiz-options-filtered {:free-text false}])}]
-                        [:label {:for "free-text-false"} "Multiple choice"]]
-                      [:form
-                        [:input {:id "from-romaji"
-                                 :name "quiz-type"
-                                 :type "radio"
-                                 :value true
-                                 :on-click #(dispatch [:quiz-options-filtered {:quiz-type "from-romaji"}])}]
-                        [:label {:for "from-romaji"} "From romaji"]
-                        [:input {:id "to-romaji"
-                                 :name "quiz-type"
-                                 :type "radio"
-                                 :value false
-                                 :on-click #(dispatch [:quiz-options-filtered {:quiz-type "to-romaji"}])}]
-                        [:label {:for "to-romaji"} "To romaji"]
-                        [:input {:id "mixed"
-                                 :name "quiz-type"
-                                 :type "radio"
-                                 :value false
-                                 :on-click #(dispatch [:quiz-options-filtered {:quiz-type "mixed"}])}]
-                        [:label {:for "mixed"} "Mixed"]]]))))
+          "quiz-options" [quiz-options]))))
 
 (defn alphabet-choice []
   (let [alphabets (subscribe [:alphabets])]
@@ -53,4 +21,37 @@
                 (second @alphabets)]]))
 
 (defn quiz-options []
+  [:div.container
+    [:form
+      [:input {:id "free-text-true"
+               :name "free-text"
+               :type "radio"
+               :value true
+               :on-click #(dispatch [:quiz-options-filtered {:free-text true}])}]
+      [:label {:for "free-text-true"} "Free text"]
+      [:input {:id "free-text-false"
+               :name "free-text"
+               :type "radio"
+               :value false
+               :on-click #(dispatch [:quiz-options-filtered {:free-text false}])}]
+      [:label {:for "free-text-false"} "Multiple choice"]]
+    [:form
+      [:input {:id "from-romaji"
+               :name "quiz-type"
+               :type "radio"
+               :value true
+               :on-click #(dispatch [:quiz-options-filtered {:quiz-type "from-romaji"}])}]
+      [:label {:for "from-romaji"} "From romaji"]
+      [:input {:id "to-romaji"
+               :name "quiz-type"
+               :type "radio"
+               :value false
+               :on-click #(dispatch [:quiz-options-filtered {:quiz-type "to-romaji"}])}]
+      [:label {:for "to-romaji"} "To romaji"]
+      [:input {:id "mixed"
+               :name "quiz-type"
+               :type "radio"
+               :value false
+               :on-click #(dispatch [:quiz-options-filtered {:quiz-type "mixed"}])}]
+      [:label {:for "mixed"} "Mixed"]]]
   )
