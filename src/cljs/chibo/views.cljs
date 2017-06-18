3374517d668b661418d9371d6a84e9f4911ccbe9
feychou <federica.recanatini@gmail.com>
2017-06-19 00:27:29 +0200

    Do not start quiz if alphabets are the same

--- src/cljs/chibo/views.cljs
+++ src/cljs/chibo/views.cljs
@@ -3,20 +3,20 @@
             [reagent.core :refer [atom]]))
 
 (defn alphabet-input [alphabet, input-name]
-  (let [quiz-options @(subscribe [:quiz-options])]
+  (let [quiz-options (subscribe [:quiz-options])]
     (fn []
       [:span
         [:input {:id (str input-name "-" alphabet)
                  :name input-name
                  :type "radio"
-                 :default-checked (= ((keyword input-name) quiz-options) alphabet)
+                 :default-checked (= ((keyword input-name) @quiz-options) alphabet)
                  :on-click #(dispatch [:quiz-options-filtered {(keyword input-name) alphabet}])
                  :value true}]
         [:label {:for (str input-name "-" alphabet)} alphabet]])))
 
 (defn quiz-options []
-  (let [quiz-options @(subscribe [:quiz-options])
-        alphabets @(subscribe [:alphabets])]
+  (let [quiz-options (subscribe [:quiz-options])
+        alphabets (subscribe [:alphabets])]
     (fn []
       [:div.container
         [:form
@@ -24,29 +24,28 @@
             [:input {:id "free-text-true"
                      :name "free-text"
                      :type "radio"
-                     :default-checked (:free-text quiz-options)
+                     :default-checked (:free-text @quiz-options)
                      :value true
                      :on-click #(dispatch [:quiz-options-filtered {:free-text true}])}]
             [:label {:for "free-text-true"} "Free text"]
             [:input {:id "free-text-false"
                      :name "free-text"
                      :type "radio"
-                     :default-checked (not (:free-text quiz-options))
+                     :default-checked (not (:free-text @quiz-options))
                      :value false
                      :on-click #(dispatch [:quiz-options-filtered {:free-text false}])}]
             [:label {:for "free-text-false"} "Multiple choice"]]]
           [:div "From"
-            (for [alphabet alphabets]
+            (for [alphabet @alphabets]
               ^{:key alphabet} [alphabet-input alphabet "from"])]
           [:div "To"
-            (for [alphabet alphabets]
+            (for [alphabet @alphabets]
               ^{:key alphabet} [alphabet-input alphabet "to"])]
           [:div
             [:button {:type "button"
-                     :on-click #(dispatch [:quiz-started])}
-                     "<<"]
-            [:button {:type "button"
-                     :on-click #(dispatch [:quiz-started])}
+                     :on-click #(if (= (:from @quiz-options) (:to @quiz-options))
+                                  (js/alert "Invalid selection")
+                                  (dispatch [:quiz-started]))}
                      ">>"]]])))
 
 (defn quiz []
